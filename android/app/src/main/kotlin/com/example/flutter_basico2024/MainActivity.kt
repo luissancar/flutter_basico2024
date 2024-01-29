007d5786b5d835dce3d523a19bb25dbbe45d84fa
luis jose sanchez carrasco <luissancar@yahoo.es>
2024-01-29 10:43:03 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_basico2024/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_basico2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiApp()); // llama al primer widget que se ejecutará
+
+/*
+void main() {
+    runApp(MiApp);
+}*/
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // es el estilo de la app
+      title: "Primera App",  // nombre de la app
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  Inicio({super.key});
+
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // estructura básica de una app
+      // estructura de una app movil
+      appBar: AppBar(
+        title: Text("Primera App Title"),
+      ),
+      body: Center(
+        child: Text("Body"),
+      ),
+    );
+  }
+}
